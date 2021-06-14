2a7ff5a365f9fb6863ed0f463ccf6b758b722fea
WataruSuzuki <wataru0406@hotmail.co.jp>
2021-06-14 22:10:00 +0900

    Created xcode project

---
+++ SimpleWidgetSuggestions/SimpleWidgetSuggestionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleWidgetSuggestionsApp.swift
+//  SimpleWidgetSuggestions
+//
+//  Created by Wataru Suzuki on 2021/06/07.
+//
+
+import SwiftUI
+
+@main
+struct SimpleWidgetSuggestionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
